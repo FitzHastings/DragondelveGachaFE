82f5cf59dd57036d3f5dfa75679d388209cd5f37
FitzHastings <Inferiordev@gmail.com>
2024-02-10 17:55:22 +0400

    Migration

---
+++ src/Gacha.js
@@ -0,0 +1,66 @@
+/* Copyright 2024 Prokhor Kalinin
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+import React from 'react';
+import NavBar from './NavBar';
+import MainContent from './MainContent';
+
+export default class Gacha extends React.Component {
+    constructor(props) {
+        super(props);
+
+        const screens = [
+            {id: 'collection', name: 'My Collection'},
+            {id: 'roll', name: 'Roll'},
+        ];
+
+        this.state = {
+            currentScreen: screens[0],
+            screens
+        };
+    }
+
+    onChangeScreen = (id) => {
+        let newCurrentScreen = 'status';
+        for (const sc of this.state.screens) {
+            if (sc.id === id) {
+                newCurrentScreen = sc;
+                console.log('Screen switch');
+                break;
+            }
+        }
+        this.setState({currentScreen: newCurrentScreen, screens: this.state.screens});
+    };
+
+    render() {
+        return (
+            <div className="column-container">
+                <div className="left-nav flex-column">
+                    <NavBar
+                        screens={this.state.screens}
+                        onChangeScreen={this.onChangeScreen}
+                    />
+                </div>
+                <div className="main-content flex-column">
+                    <MainContent
+                        currentScreen={this.state.currentScreen}
+                    />
+                </div>
+                <div className="right-pad flex-column">
+                </div>
+            </div>
+        );
+    }
+}
---
+++ src/NavBar.js
@@ -0,0 +1,54 @@
+/* Copyright 2024 Prokhor Kalinin
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+import NavButton from './controls/NavButton';
+import React from 'react';
+
+export default class NavBar extends React.Component {
+    constructor(props) {
+        super(props);
+        const navButtons = this.props.screens.map(screen => Object.assign({}, screen, {isToggled: false}));
+        navButtons[0].isToggled = true;
+        this.state = {navButtons};
+    }
+
+    onToggle = (id) => {
+        const newButtons = this.state.navButtons.map(navButton => {
+            if (navButton.id === id) return Object.assign({}, navButton, {isToggled: true});
+            return Object.assign({}, navButton, {isToggled: false});
+        });
+        this.props.onChangeScreen(id);
+        this.setState({navButtons: newButtons});
+    };
+
+    render() {
+        const navComponents = this.state.navButtons.map((navButton) => (
+            <div key={'nav-' + navButton.id}>
+                <NavButton
+                    id={navButton.id}
+                    text={navButton.name}
+                    isToggled={navButton.isToggled}
+                    onToggle={this.onToggle}
+                />
+            </div>
+        ));
+
+        return (
+            <div className="padded-container">
+                {navComponents}
+            </div>
+        );
+    }
+}
