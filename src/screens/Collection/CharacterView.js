8b8af16ca5f366c8b587b95aa6d9d639ec2577b2
FitzHastings <Inferiordev@gmail.com>
2024-03-13 21:44:42 +0400

    Collection Details stubs added

---
+++ src/screens/Collection/CharacterView.js
@@ -0,0 +1,30 @@
+/* Copyright 2024 Prokhor Kalinin
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+import React from 'react';
+
+export default class CollectionScreen extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+        <div className='scale first-layer full-width'>
+            <span className='scale-title-label'>this.props.name</span>
+        </div>
+        );
+    }
+}
